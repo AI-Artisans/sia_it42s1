80ccf25c3ea3474e69c8f36d2b813f889e05ddce
koyakev <kevinyu2201@gmail.com>
2025-04-26 23:30:20 +0800

    matulog at magpahinga, at wag kakalimutang manalangin at magpasalamat

--- KLRYu/20250426-webapp/application/controllers/Webservice.php
+++ KLRYu/20250426-webapp/application/controllers/Webservice.php
@@ -26,24 +26,8 @@ class Webservice extends CI_Controller {
     }
 
     public function consume(){
-        $apiKey = "0f19cda9217560805fd1a4a59b649e64";
-        $location = "Philippines";
-        $apiUrl = "http://api.weatherstack.com/current?";
-        $apiUrl .= "access_key={$apiKey}&query={$location}";
-
-        // Initialize cURL session
-        $ch = curl_init();
-        // Set cURL options
-        curl_setopt($ch, CURLOPT_URL, $apiUrl); curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        // Execute cURL session and store the response
-        $response = curl_exec($ch);
-
-        // Close cURL session curl_close($ch);
-        // Decode the JSON response
-
-        $weatherData = json_decode($response, true);
-
-        $data['weatherData'] = $weatherData;
+        
+        $data['weatherData'] = $this->getWSdata();
 
         $this->load->view('wsconsume', $data);
     }
@@ -59,6 +43,5 @@ class Webservice extends CI_Controller {
         print_r($wsdata);
         }
     }
-        
 }
 ?>
